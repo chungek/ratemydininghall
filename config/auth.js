fb35c70cf3b9a5822032af4faa824717bf129e09
Ek Kheng Chung <chungek@brandeis.edu>
2018-06-26 14:26:27 -0400

    callback url change

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '850085423377-bp589qecett1o1p54pn1vlldhubk95gt.apps.googleusercontent.com',
         'clientSecret'  : '3ks2TazLpI7nZoEM_v3HCvVU',
-        'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
+        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:5500/login/authorized'
     }
 
 };
